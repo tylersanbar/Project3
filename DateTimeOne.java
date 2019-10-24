0ff747b7c8f5c79097f1783eb791b1256a328834
Tyler Sanbar <tylersanbar@ou.edu>
2019-10-23 23:05:25 -0500

    Finished dateTimeOfOtherCity

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -1,29 +1,31 @@
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.TimeZone;
 
 public class DateTimeOne extends MesoDateTimeOneAbstract
 {
-	private Calendar date;
+	private Calendar dateServer;
 	//date formatted as 10/10/2019 06:24 PM
 	SimpleDateFormat format = new SimpleDateFormat("M/d/YYYY h:mm a");
+	String dateFormat;
+
+
 	
-	DateTimeOne() {
-		date = Calendar.getInstance();
+	public DateTimeOne() {
+		dateServer = Calendar.getInstance();
+		dateFormat = format.format(dateServer.getTime());
 	}
 	
 	@Override
-	int getValueOfSecond() {
-		int second = date.get(Calendar.SECOND);
+	public int getValueOfSecond() {
+		int second = dateServer.get(Calendar.SECOND);
 		System.out.println("The value of Second now: " + second);
 		return second;
 	}
 
 	@Override
-	void dateTimeNow() {
-		date = Calendar.getInstance();
-
-		String dateFormat = format.format(date.getTime());
-
+	public void dateTimeNow() {
+		new DateTimeOne();
 		System.out.println("Current Date/Time " + dateFormat);
 	}
 
@@ -37,10 +39,26 @@ public class DateTimeOne extends MesoDateTimeOneAbstract
 		}
 		
 	}
-
+/**			Time on Server: 15:14
+			GMT: 20:14
+			BST (90E): 2:14
+			CST (90W): 15:14
+			**/
 	@Override
 	void dateTimeOfOtherCity() {
-		// TODO Auto-generated method stub
+		SimpleDateFormat cityFormat = new SimpleDateFormat("H:mm");
+		String formatServer = cityFormat.format(dateServer.getTime());
+		
+		cityFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+		String formatGMT = cityFormat.format(dateServer.getTime());
+		
+		cityFormat.setTimeZone(TimeZone.getTimeZone("BST"));
+		String formatBST = cityFormat.format(dateServer.getTime());
+		
+		cityFormat.setTimeZone(TimeZone.getTimeZone("CST"));
+		String formatCST = cityFormat.format(dateServer.getTime());
+		
+		System.out.format("Time on Server: %1s\nGMT: %2s\nBST (90E): %5s\nCST (90W): %4s", formatServer, formatGMT, formatBST, formatCST);
 		
 	}
 
